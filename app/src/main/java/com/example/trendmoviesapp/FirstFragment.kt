5a723ed858a2ea43f172d2b30625ffb743242ae6
MohamedKedra <mohamed.a.kedra@gmail.com>
2022-11-13 17:30:54 +0200

    design list & movie item & loading layout

--- app/src/main/java/com/example/trendmoviesapp/FirstFragment.kt
+++ app/src/main/java/com/example/trendmoviesapp/FirstFragment.kt
@@ -8,15 +8,10 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.trendmoviesapp.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -32,9 +27,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
+
     }
 
     override fun onDestroyView() {
