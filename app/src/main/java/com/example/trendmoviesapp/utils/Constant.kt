c1a71c340584a01e6110febf34711bf293e18c18
MohamedKedra <mohamed.a.kedra@gmail.com>
2022-11-14 15:43:39 +0200

    fix error in images

--- app/src/main/java/com/example/trendmoviesapp/utils/Constant.kt
+++ app/src/main/java/com/example/trendmoviesapp/utils/Constant.kt
@@ -4,5 +4,5 @@ object Constant {
 
     const val baseUrl = "https://api.themoviedb.org/3/"
     const val apiKey = "c9856d0cb57c3f14bf75bdc6c063b8f3"
-    const val imageBase = "http://image.tmdb.org/t/p/w500/"
+    const val imageBase = "https://image.tmdb.org/t/p/w342/"
 }
