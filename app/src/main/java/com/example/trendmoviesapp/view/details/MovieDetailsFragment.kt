37a8a39c05f5820004fa258c895c6e559154d558
MohamedKedra <mohamed.a.kedra@gmail.com>
2022-11-14 17:55:42 +0200

    design details fragment

---
+++ app/src/main/java/com/example/trendmoviesapp/view/details/MovieDetailsFragment.kt
@@ -0,0 +1,34 @@
+package com.example.trendmoviesapp.view.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.trendmoviesapp.R
+import com.example.trendmoviesapp.databinding.FragmentMovieDetailsBinding
+
+class MovieDetailsFragment : Fragment() {
+
+    private var binding: FragmentMovieDetailsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+        return binding?.root!!
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
